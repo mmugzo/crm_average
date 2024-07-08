d29b7eeefcae466a0116e28e2313b2992b11a81f
mmugzo <124833709+mmugzo@users.noreply.github.com>
2024-07-08 18:29:09 +0300

    Update p65_score.py

--- p65_score.py
+++ p65_score.py
@@ -1,4 +1,4 @@
-# 
+# dependencies field
 dependencies = x_studio_average_score, 
                 x_studio_p65_eos_life, 
                 x_studio_p65_know_like_trust,
@@ -7,6 +7,6 @@ dependencies = x_studio_average_score,
                 x_studio_p65_monthly_income,
                 x_studio_p65_long_term_income
     
-# computed code
+# computed field
 for record in self:
     record['x_studio_average_score'] = (int(record.x_studio_p65_eos_life) + int(record.x_studio_p65_know_like_trust) + int(record.x_studio_p65_location_independent) + int(record.x_studio_p65_no_inventory) + int(record.x_studio_p65_monthly_income) + int(record.x_studio_p65_long_term_income)) / 6
